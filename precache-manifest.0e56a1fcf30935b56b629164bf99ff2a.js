c7ce2f4946c598ad5b768c61b50321d142f79977
Jordan Pearson <jsantipearson@gmail.com>
2021-01-31 19:30:24 -0800

    Updates

---
+++ precache-manifest.0e56a1fcf30935b56b629164bf99ff2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91943ce2b13f4f507ac7",
+    "url": "/home/static/css/main.1cfad3df.chunk.css"
+  },
+  {
+    "revision": "91943ce2b13f4f507ac7",
+    "url": "/home/static/js/main.91943ce2.chunk.js"
+  },
+  {
+    "revision": "5fdf7f86495bc64854ab",
+    "url": "/home/static/js/runtime~main.5fdf7f86.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/home/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "215f66ff091c8094212489578bea7e0e",
+    "url": "/home/index.html"
+  }
+];
