41b0cf2601454c67afbdbdef2bb4c634ce691ff3
Jordan Pearson <jsantipearson@gmail.com>
2021-02-01 20:59:19 -0800

    Updates

---
+++ precache-manifest.8f064cd9ecdd3512b1d6eea72aee3f89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "beacfd69b233c38a3eca",
+    "url": "/static/css/main.1cfad3df.chunk.css"
+  },
+  {
+    "revision": "beacfd69b233c38a3eca",
+    "url": "/static/js/main.beacfd69.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "573698287b10209aaf23",
+    "url": "/static/js/2.57369828.chunk.js"
+  },
+  {
+    "revision": "defb7984a359a81507b07dbfd3bc4dd7",
+    "url": "/static/media/resume.defb7984.pdf"
+  },
+  {
+    "revision": "bcf8862c7fe6cbfc028c58d6ca4aad90",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/landing/precache-manifest.4408a2b2cdb091f675cd6efbba2c1d15.js"
+  "/precache-manifest.8f064cd9ecdd3512b1d6eea72aee3f89.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/landing/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
